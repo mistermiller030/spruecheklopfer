824c56efab3d77688e7b0154fdfc949c05a6bac8
mistermiller030 <100713695+mistermiller030@users.noreply.github.com>
2022-06-16 20:00:40 +0200

    des Update mit Backend!

---
+++ NODE-REST-API/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+dotenv.config();
+
+
+//Baut die MongoDB connection auf
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("connected to mongoDB!")
+      } catch (error) {
+        throw error
+      }
+};
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongo disconected");
+})
+
+mongoose.connection.on("connected", ()=>{
+    console.log("mongo connected");
+})
+//---------------------------//
+
+//middleware
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
+
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+
+app.listen(8800, () =>{
+    connect()
+    console.log("Backend Server is running")
+})
